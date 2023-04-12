070e158770d171f970ffe6e7b327206baa204e26
mugip <76488298+mugiply@users.noreply.github.com>
2023-04-13 04:59:07 +0900

    fix: JSON API のURL変更

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   SHOPPING_LIST_JSON_API_URL:
-    'https://script.google.com/macros/s/AKfycbxG8BUqph9H7kmUqA5hGBxWwENfySVfdQBjtuZJQWLhCuC7wYYG1d0Hmc-jGrFUwBvWbg/exec',
+    'https://script.google.com/macros/s/AKfycbzqdzs-782d8b0FIVlV0zcDTEkFo65BaGtc212VO04FyOUH8wNs_nQiV4O6KmimQMLTUA/exec',
 };
