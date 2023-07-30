c47b6456860b515d97328616d4157291a326163b
mugip <76488298+mugiply@users.noreply.github.com>
2023-07-30 17:53:32 +0900

    feat: 誕生日バナー

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'arisucool';
+
+  isBirthday = false;
+
+  ngOnInit() {
+    const date = new Date();
+    // 7月31日の場合
+    if (date.getMonth() === 6 && date.getDate() === 31) {
+      this.isBirthday = true;
+    }
+  }
 }
